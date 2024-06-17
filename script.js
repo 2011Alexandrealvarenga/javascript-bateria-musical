a5605756b265bd4e519289218dda712ad9ed7e6a
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-17 11:35:07 -0300

    identifica qual a tecla pressionada, exemplo keya (tecla a)

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // evento entende o que foi pressionado e solto ao digitar
-document.body.addEventListener('keyup')
+document.body.addEventListener('keyup', (event)=>{
+  // identifica qual a tecla pressionada, exemplo keya (tecla a) 
+  console.log(event.code);
+})
