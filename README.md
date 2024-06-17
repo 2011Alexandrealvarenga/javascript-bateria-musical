edb80af6b4b6d72947e9e73bb05f924f4468df64
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-17 11:51:03 -0300

    verifica a tecla digitada e adiciona a classe

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # javascript-bateria-musical
 javascript bateria musical
 
+https://alunos.b7web.com.br/curso/javascript/projeto-1-bateria
+
  <img src="Capturar.PNG" alt="">
--- script.js
+++ script.js
@@ -10,8 +10,16 @@ function playSound(sound){
   // sound é a tecla que foi pressionada
   let audioElement = document.querySelector(`#s_${sound}`);
 
+  // pega a tecla digitada
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+
   // se tiver o audio relacionado a tecla vai tocar
   if(audioElement){
     audioElement.play();
+  }
+
+  // verifica a tecla digitada e adiciona a classe
+  if(keyElement){
+    keyElement.classList.add('active');
   }
 }
