4a08d352704da8bd03ff94de5d4c9e95183f5ee2
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-17 14:58:47 -0300

    executa a musica com intevalos

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ document.body.addEventListener('keyup', (event)=>{
   playSound(event.code.toLowerCase());
 });
 
+// pega o que foi digitado 
+document.querySelector('.composer button').addEventListener('click', ()=>{
+  let song = document.querySelector('#input').value;
+  if(song !==''){
+
+    // transforma em array
+    let songArray = song.split('');
+    playComposition(songArray);
+  }
+});
+
 function playSound(sound){
   // sound é a tecla que foi pressionada
   let audioElement = document.querySelector(`#s_${sound}`);
@@ -15,11 +26,28 @@ function playSound(sound){
 
   // se tiver o audio relacionado a tecla vai tocar
   if(audioElement){
+    // insere um valor no tempo
+    audioElement.currentTime = 0;
     audioElement.play();
   }
 
   // verifica a tecla digitada e adiciona a classe
   if(keyElement){
     keyElement.classList.add('active');
+    setTimeout(()=>{
+      keyElement.classList.remove('active');
+
+    },300);
+  }
+}
+// executa a musica com intevalos
+function playComposition(songArray){
+  let wait = 0;
+  for(let songItem of songArray){
+    setTimeout(()=>{
+      playSound(`key${songItem}`);
+    }, wait);
+
+    wait += 250;
   }
 }
