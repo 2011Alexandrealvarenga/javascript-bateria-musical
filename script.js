0e239b1ed221ba20b3d544b992e5d3f018f3ea43
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-06-17 11:43:32 -0300

    se tiver o audio relacionado a tecla vai tocar

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // evento entende o que foi pressionado e solto ao digitar
 document.body.addEventListener('keyup', (event)=>{
   // identifica qual a tecla pressionada, exemplo keya (tecla a) 
-  console.log(event.code);
-})
+  // console.log(event.code);
+
+  playSound(event.code.toLowerCase());
+});
+
+function playSound(sound){
+  // sound é a tecla que foi pressionada
+  let audioElement = document.querySelector(`#s_${sound}`);
+
+  // se tiver o audio relacionado a tecla vai tocar
+  if(audioElement){
+    audioElement.play();
+  }
+}
